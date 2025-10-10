d28d7715c2979d425fa2069a5fa42d8ea1f50c6d
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2025-10-10 16:38:44 +0000

    added a nav include all pages for global navigation

---
+++ lesson-6/includes/nav.php
@@ -0,0 +1,8 @@
+	<nav>
+		<ul>
+			<li><a href="/lesson-6/index.php" title="Go to the Home page">Home</a></li>
+			<li><a href="/lesson-6/red.php" title="Go to the Red page">Red</a></li>
+			<li><a href="/lesson-6/blue.php" title="Go to the Blue page">Blue</a></li>
+			<li><a href="/lesson-6/yellow.php" title="Go to the Yellow page">Yellow</a></li>
+		</ul>
+	</nav>
